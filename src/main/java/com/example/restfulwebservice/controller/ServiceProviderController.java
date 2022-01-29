de47edcbf70ea6dcb89c1967f5b84a6ebe98aef5
Megi <mkceva@gmail.com>
2022-01-29 23:37:50 +0100

    adding two GET endpoints welcome,getMessage with name param

---
+++ src/main/java/com/example/restfulwebservice/controller/ServiceProviderController.java
@@ -0,0 +1,29 @@
+package com.example.restfulwebservice.controller;
+
+import com.example.restfulwebservice.model.GreetingMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class ServiceProviderController {
+
+    public final static String MESSAGE = "This is the resulting displaying message for %s";
+    public final AtomicLong counter = new AtomicLong();
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome!!!";
+    }
+
+
+    @GetMapping("/greeting")
+    public GreetingMessage getMessage(
+            @RequestParam(value = "name", defaultValue = "Megi",required = false) String name){
+
+        return new GreetingMessage(counter.incrementAndGet(), String.format(MESSAGE, name));
+    }
+
+}
